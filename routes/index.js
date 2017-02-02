0ee9bb3acb70299651c91693b757707614d3a651
Rui Chuang <rui@FL-rui-mbp.local>
2017-02-02 01:40:49 -0500

    session management, data formate

--- routes/index.js
+++ routes/index.js
@@ -4,39 +4,65 @@ var fs = require('fs');
 
 var dlInfo;
 var fname, lname, expirationDate, dob, issueDate, postCode, sex, address, city, province, dlNum;
+var sessionKey;
+var dataSet = {};
+
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Driver Licence Info Display',fname: fname, lname: lname, expirationDate: expirationDate, dob: dob, issueDate: issueDate, postCode: postCode, sex: sex, address: address, city: city, province: province, dlNum: dlNum });
+
+  console.log(dataSet);
+  var key = req.query.key;
+
+  if (key) {
+    var data = dataSet[key];
+    // console.log(data);
+
+    var regex1 = /DBA(.*)\nDCS(.*)\nDCT(.*)\nDBD(.*)\nDBB(.*)\nDBC(.*)\n/g;
+    var regex2 = /DAG(.*)\n/g; //DAI(.*)\nDAJ(.*)\nDAQ(.*)\n
+    var regex3 = /DAI(.*)\nDAJ(.*)\n/g;
+    var regex4 = /DAQ(.*)\n/g;
+    var match1 = regex1.exec(data);
+    var match2 = regex2.exec(data);
+    var match3 = regex3.exec(data);
+    var match4 = regex4.exec(data);
+    expirationDate = [match1[1].slice(0, 4), '-', match1[1].slice(4, 6), '-', match1[1].slice(6)].join('');;
+    lname = match1[2].replace(/,/g, '');
+    fname = match1[3];
+    issueDate = [match1[4].slice(0, 4), '-', match1[4].slice(4, 6), '-', match1[4].slice(6)].join('');
+    dob = [match1[5].slice(0, 4), '-', match1[5].slice(4, 6), '-', match1[5].slice(6)].join('');;
+    if (match1[6] === '1') {
+      sex = 'Male';
+    } else {
+      sex = 'Female'
+    }
+    address = match2[1].replace(/,/g, '');
+    city = match3[1];
+    province = match3[2];
+    dlNum = match4[1];
+
+
+    res.render('index', { title: 'Driver Licence Info Display',fname: fname, lname: lname, expirationDate: expirationDate, dob: dob, issueDate: issueDate, postCode: postCode, sex: sex, address: address, city: city, province: province, dlNum: dlNum });
+
+  } else {
+    res.render('error', { message: 'No data to present... Please scan a Driver Licence first!'});
+  }
+
+
 
 });
 
 router.post('/', function (req, res, next) {
 
-  console.log(req.body);
   dlInfo = req.body.dlInfo;
+  sessionKey = req.body.key;
+  dataSet[sessionKey] = dlInfo;
 
-  var regex1 = /DBA(.*)\nDCS(.*)\nDCT(.*)\nDBD(.*)\nDBB(.*)\nDBC(.*)\n/g;
-  var regex2 = /DAG(.*)\n/g; //DAI(.*)\nDAJ(.*)\nDAQ(.*)\n
-  var regex3 = /DAI(.*)\nDAJ(.*)\n/g;
-  var regex4 = /DAQ(.*)\n/g;
-  var match1 = regex1.exec(dlInfo);
-  var match2 = regex2.exec(dlInfo);
-  var match3 = regex3.exec(dlInfo);
-  var match4 = regex4.exec(dlInfo);
-  expirationDate = match1[1];
-  lname = match1[2];
-  fname = match1[3];
-  issueDate = match1[4];
-  dob = match1[5];
-  sex = match1[6];
-  address = match2[1];
-  console.log(address);
-  city = match3[1];
-  province = match3[2];
-  dlNum = match4[1];
+  res.send('goood!')
+  //console.log(dataSet);
 
 })
+
 
 module.exports = router;
